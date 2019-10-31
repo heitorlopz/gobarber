0f0abe101c71eaa1718f3bec6299be02c6925e3c
heitorlopz <fheitorlopes@gmail.com>
2019-10-31 11:28:06 -0300

    setting multer

--- src/routes.js
+++ src/routes.js
@@ -1,12 +1,16 @@
 /* aqui vamos importar o Router e não o express inteiro, pois é uma forma de
 separar a parte de roteamento do express em outro arquivo */
 import { Router } from 'express';
+import multer from 'multer';
+import multerConfig from './config/multer';
+
 import UserController from './app/controllers/UserController';
 import SessionController from './app/controllers/SessionController';
 
 import authMiddleware from './app/middlewares/auth';
 
 const routes = new Router();
+const upload = multer(multerConfig);
 
 routes.post('/users', UserController.store);
 routes.post('/sessions', SessionController.store);
@@ -15,6 +19,10 @@ routes.post('/sessions', SessionController.store);
 routes.use(authMiddleware);
 
 routes.put('/users', UserController.update);
+
+routes.post('/files', upload.single('file'), (req, res) => {
+  return res.json({ ok: true });
+});
 
 // toda operação que fazemos no banco de dados é assincrona, ou seja, não acontece em tempo real, então precisamos utilizar o async/await
 /*  routes.get('/', async (req, res) => {
