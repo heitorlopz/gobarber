29c34959aa2b7200d7fc74df96612031e80fcfcc
heitorlopz <fheitorlopes@gmail.com>
2019-11-02 17:55:40 -0300

    setting nodemailer && deleting appointments

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: '25366967f06cd9',
+    pass: '59e50bb8397bcc',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
